5e36fa81c4bbc0576e98161fafc6cc6e551c50a7
Juliana <julianaxpontes@gmail.com>
2026-04-16 14:00:26 -0300

    feat: adiciona testes unitarios

--- tests/tests/test_app.py
+++ tests/tests/test_app.py
@@ -1,17 +1,22 @@
 from app import soma, subtrai, multiplica, divide
 import pytest
 
+
 def test_soma():
     assert soma(2, 3) == 5
 
+
 def test_subtrai():
-    assert subtrai(5, 3) == 2
+    assert subtrai(10, 4) == 6
+
 
 def test_multiplica():
-    assert multiplica(2, 4) == 8
+    assert multiplica(3, 5) == 15
+
 
 def test_divide():
-    assert divide(10, 2) == 5
+    assert divide(8, 2) == 4
+
 
 def test_divide_por_zero():
     with pytest.raises(ValueError):
